a1ca3486d2893f05653d25cc5ecd2b70dbb9111a
Addison <addison@framer.com>
2019-09-03 16:59:52 +0200

    Finish Alerts

---
+++ code/Alerts/InlineAlert.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import { addPropertyControls, ControlType } from "framer";
+
+type Props = {
+  height: number;
+  width: number;
+  kind: string;
+  text: string;
+  context: string;
+};
+
+export function InlineAlert(props: Props) {
+  return (
+    <div
+      className={`alert ${props.kind} is-inline`}
+      style={{ width: "100%", padding: "10px" }}
+    >
+      <p>
+        <span className={"is-bold"}>{props.text}</span>
+        {props.context}
+      </p>
+    </div>
+  );
+}
+
+addPropertyControls(InlineAlert, {
+  kind: {
+    type: ControlType.Enum,
+    title: "Kind",
+    options: [
+      "has-bg-status-default",
+      "has-bg-status-info",
+      "has-bg-status-success",
+      "has-bg-status-warning",
+      "has-bg-status-danger"
+    ],
+    optionTitles: ["Default", "Info", "Success", "Warning", "Danger"],
+    defaultValue: "has-bg-status-default"
+  },
+  text: {
+    type: ControlType.String,
+    title: "Bold Text",
+    defaultValue: "Non-Production Instance: "
+  },
+  context: {
+    type: ControlType.String,
+    title: "text",
+    defaultValue: "Do not use for Production or CI/CD (Screwdriver)"
+  }
+});
+
+InlineAlert.defaultProps = {
+  height: 45,
+  width: 800
+};
