aad53d2918acda91dc3ebcfaf06b5268add38348
Addison <addison@framer.com>
2019-09-04 16:13:26 +0200

    Finish Select

--- code/Select.tsx
+++ code/Select.tsx
@@ -1,20 +1,43 @@
 import * as React from "react";
 import { addPropertyControls, ControlType } from "framer";
 
-export function Select() {
+type Props = {
+  height: number;
+  width: number;
+  text: string;
+  options: string[];
+};
+
+export function Select(props: Props) {
   return (
     <div className="input has-arrow">
       <select name="">
-        <option value="" disabled="" selected="">
-          Select your option
-        </option>
-
-        <option value="">Value 1</option>
-
-        <option value="">Value 2</option>
-
-        <option value="">Value 3</option>
+        <option value="">{props.text}</option>
+        {props.options.map(option => {
+          return <option value="">{option}</option>;
+        })}
       </select>
     </div>
   );
 }
+
+addPropertyControls(Select, {
+  text: {
+    type: ControlType.String,
+    title: "Text",
+    defaultValue: "Select an option"
+  },
+  options: {
+    type: ControlType.Array,
+    propertyControl: {
+      type: ControlType.String
+    },
+    title: "Options",
+    defaultValue: ["Value 1", "Value 2"]
+  }
+});
+
+Select.defaultProps = {
+  width: 250,
+  height: 36
+};
