e797f4b6ebb4bc338c67d49636c5f48221e6207f
Addison <addison@framer.com>
2019-09-04 15:20:25 +0200

    Finish menu

--- code/Loader.tsx
+++ code/Loader.tsx
@@ -1,6 +1,45 @@
 import * as React from "react";
 import { addPropertyControls, ControlType } from "framer";
 
-export function Loader() {
-  return <div className={"loader is-large"}></div>;
+type Props = {
+  height: number;
+  width: number;
+  inverse: boolean;
+  size: string;
+};
+
+export function Loader(props: Props) {
+  return (
+    <div
+      className={`loader ${props.inverse && "is-inverse"} ${props.size}`}
+    ></div>
+  );
 }
+
+addPropertyControls(Loader, {
+  inverse: {
+    type: ControlType.Boolean,
+    title: "Inverse",
+    defaultValue: false,
+    enabledTitle: "True",
+    disabledTitle: "False"
+  },
+  size: {
+    type: ControlType.Enum,
+    title: "Size",
+    options: [
+      "is-extrasmall",
+      "is-small",
+      "is-default",
+      "is-medium",
+      "is-large"
+    ],
+    optionTitles: ["XS", "Small", "Default", "Medium", "Large"],
+    defaultValue: "is-default"
+  }
+});
+
+Loader.defaultProps = {
+  height: 24,
+  width: 24
+};
--- code/Menu.tsx
+++ code/Menu.tsx
@@ -1,24 +1,81 @@
 import * as React from "react";
 import { addPropertyControls, ControlType } from "framer";
 
-export function Menu() {
+type Props = {
+  height: number;
+  width: number;
+  text: string;
+  items: string[];
+  align: string;
+  showFooter: boolean;
+  footerItems: string[];
+};
+
+export function Menu(props: Props) {
   return (
-    <div className="menu">
-      <a className="menu-trigger">Hover for default menu</a>
+    <div className={`menu ${props.align}`} style={{ margin: 20 }}>
+      <a className="menu-trigger">{props.text}</a>
 
       <div className="menu-content">
-        <a>Home</a>
-
-        <a>About</a>
-
-        <a>Contact</a>
-
-        <a>Help</a>
-
-        <hr />
-
-        <a>Support</a>
+        {props.items &&
+          props.items.map(item => {
+            return <a>{item}</a>;
+          })}
+        {props.showFooter && (
+          <>
+            <hr />
+            {props.footerItems.map(item => {
+              return <a>{item}</a>;
+            })}
+          </>
+        )}
       </div>
     </div>
   );
 }
+
+addPropertyControls(Menu, {
+  text: {
+    type: ControlType.String,
+    title: "Text",
+    defaultValue: "Hover Me"
+  },
+  items: {
+    type: ControlType.Array,
+    title: "Menu Items",
+    propertyControl: {
+      type: ControlType.String
+    },
+    defaultValue: ["Item 1", "Item 2"]
+  },
+  align: {
+    type: ControlType.Enum,
+    title: "Alignment",
+    options: ["is-left", "is-default", "is-right"],
+    optionTitles: ["Left", "Default", "Right"],
+    defaultValue: "is-default"
+  },
+  showFooter: {
+    type: ControlType.Boolean,
+    title: "Footer",
+    defaultValue: false,
+    enabledTitle: "True",
+    disabledTitle: "False"
+  },
+  footerItems: {
+    type: ControlType.Array,
+    title: "Footer Items",
+    propertyControl: {
+      type: ControlType.String
+    },
+    defaultValue: ["Footer"],
+    hidden(props) {
+      return props.showFooter == false;
+    }
+  }
+});
+
+Menu.defaultProps = {
+  height: 215,
+  width: 110
+};
