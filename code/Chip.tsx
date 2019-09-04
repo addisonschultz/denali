32c314f4a146afd9ad5e1e583e339143f34b3b18
Addison <addison@framer.com>
2019-09-04 14:41:07 +0200

    Finish Field

--- code/Chip.tsx
+++ code/Chip.tsx
@@ -1,6 +1,98 @@
 import * as React from "react";
 import { addPropertyControls, ControlType } from "framer";
 
-export function Chip() {
-  return <span className={"chips"}>Default Chip</span>;
+type Props = {
+  height: number;
+  width: number;
+  text: string;
+  color: string;
+  weight: string;
+};
+
+export function Chip(props: Props) {
+  return (
+    <span className={`chips has-bg-${props.color}-${props.weight}`}>
+      {props.text}
+    </span>
+  );
 }
+
+addPropertyControls(Chip, {
+  text: {
+    type: ControlType.String,
+    title: "Text",
+    defaultValue: "Default Chip"
+  },
+  color: {
+    type: ControlType.Enum,
+    title: "Color",
+    options: [
+      "default",
+      "red",
+      "orange",
+      "yellow",
+      "lime",
+      "green",
+      "sky",
+      "blue",
+      "purple",
+      "violet",
+      "pink"
+    ],
+    optionTitles: [
+      "Default",
+      "Red",
+      "Orange",
+      "Yellow",
+      "Lime",
+      "Green",
+      "Sky",
+      "Blue",
+      "Purple",
+      "Violet",
+      "Pink"
+    ]
+  },
+  weight: {
+    type: ControlType.Enum,
+    title: "Weight",
+    options: [
+      "100",
+      "200",
+      "300",
+      "400",
+      "500",
+      "600",
+      "700",
+      "800",
+      "900",
+      "r100",
+      "r200",
+      "r300",
+      "r400",
+      "r500"
+    ],
+    optionTitles: [
+      "100",
+      "200",
+      "300",
+      "400",
+      "500",
+      "600",
+      "700",
+      "800",
+      "900",
+      "r100",
+      "r200",
+      "r300",
+      "r400",
+      "r500"
+    ],
+    defaultValue: "500"
+  }
+});
+
+Chip.defaultProps = {
+  height: 20,
+  width: 95
+};
---
+++ code/Icon.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { addPropertyControls, ControlType } from "framer";
+
+export function Icon() {
+  return <div></div>;
+}
