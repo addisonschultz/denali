67b64adc3a7a960a7cf1c23d30f39ced9f67a369
Addison <addison@framer.com>
2019-09-11 13:43:09 +0200

    Updated Shared Colors

--- code/canvas.tsx
+++ code/canvas.tsx
@@ -1,96 +1,66 @@
 // WARNING: this file is auto generated, any changes will be lost
-import { createDesignComponent, CanvasStore } from "framer";
+import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
 
 export const colors = Object.freeze({
-  /** #3448F7 */
-  "Brand-800":
-    "var(--token-fc8164b6-d6d5-4723-a92d-63344a4c4e54, rgb(52, 72, 247))",
-  /** #3570F4 */
-  "Brand-700":
-    "var(--token-fd7c06c7-1111-416f-a296-167e1a71e9d4, rgb(53, 112, 244))",
-  /** #3697F2 */
-  "Brand-600":
-    "var(--token-0ef2e91a-1cbc-43e0-88dc-e6daeb15cfb2, rgb(54, 151, 242))",
-  /** #8AC5ED */
-  "Brand-500":
-    "var(--token-547c6762-8922-4b5b-ad03-a7fd32205012, rgb(138, 197, 237))",
-  /** #5D8CF6 */
-  "Brand-400":
-    "var(--token-aacdf6d7-7551-4fcb-9d4e-ea5bca1abda2, rgb(93, 140, 246))",
-  /** #9AB7F9 */
-  "Brand-300":
-    "var(--token-91483418-1b65-4975-97ad-7c19aa99b58b, rgb(154, 183, 249))",
-  /** #D7E2FD */
-  "Brand-200":
-    "var(--token-685a4c4a-e602-485c-920b-e64961b65665, rgb(215, 226, 253))",
-  /** #F5F8FE */
-  "Brand-100":
-    "var(--token-6b0117bc-8f17-49ed-8418-0268177d1e2b, rgb(245, 248, 254))",
-  /** #303030 */
-  "Grey-800":
-    "var(--token-c73b0c17-d87a-4ba0-ab8d-da8e7458b812, rgb(48, 48, 48))",
-  /** #606060 */
-  "Grey-700":
-    "var(--token-811cf929-2816-431c-8318-538ec9a672af, rgb(96, 96, 96))",
-  /** #9A9A9A */
-  "Grey-600":
-    "var(--token-516b268b-f430-4151-b71c-d27c9151a041, rgb(154, 154, 154))",
-  /** #D5D5D5 */
-  "Grey-500":
-    "var(--token-c327336c-a218-4c58-a913-e7a02f98d873, rgb(213, 213, 213))",
-  /** #E8E8E8 */
-  "Grey-400":
-    "var(--token-0708c948-4d4e-48e5-b251-12f8fa65d757, rgb(232, 232, 232))",
-  /** #F2F2F2 */
-  "Grey-300":
-    "var(--token-9cca215a-f3d5-4be8-8d70-2041be5c650b, rgb(242, 242, 242))",
-  /** #F8F8F8 */
-  "Grey-200":
-    "var(--token-5963099e-e002-42d6-8c50-ab3437d0d013, rgb(248, 248, 248))",
-  /** #FFFFFF */
-  "Grey-100":
-    "var(--token-ed699c54-f469-48a4-be1e-7acb87dae6cc, rgb(255, 255, 255))",
-  /** #EA0000 */
-  "Status-Danger":
-    "var(--token-fbe22f75-f9d7-4adf-bc9d-58819d9cbff8, rgb(234, 0, 0))",
-  /** #0066DF */
-  "Status-Information":
-    "var(--token-aabbc209-a498-49ee-aa2a-d70ee5af7d78, rgb(0, 102, 223))",
-  /** #15C046 */
-  "Status-Success":
-    "var(--token-ec742ce2-4236-4d94-b70d-09268106a774, rgb(21, 192, 70))",
-  /** #F4CB00 */
-  "Status-Warning":
-    "var(--token-1382129d-8c1a-4466-a66b-15d29e03cf39, rgb(244, 203, 0))",
-  /** #E21717 */
-  "Red-500":
-    "var(--token-f0e6d1bc-51a8-4d72-ac1c-30fcb0907de2, rgb(226, 23, 23))",
-  /** #F17603 */
-  "Orange-500":
-    "var(--token-1db2dfb1-c8c9-46aa-a37c-176d6133408b, rgb(241, 118, 3))",
-  /** #FED800 */
-  "Yellow-500":
-    "var(--token-45d5ee4c-2aa1-4ef1-8a7c-2cdc4f992f7d, rgb(254, 216, 0))",
-  /** #87D812 */
-  "Lime-500":
-    "var(--token-603e6c61-4e1b-4660-b7ea-6f2991afb963, rgb(135, 216, 18))",
-  /** #20C05B */
-  "Green-500":
-    "var(--token-370818ad-8c20-4b2a-ad47-de90b4701356, rgb(32, 192, 91))",
-  /** #19C6F4 */
-  "Sky-500":
-    "var(--token-9afc70b6-9467-494e-bbbf-a2fc91425fa0, rgb(25, 198, 244))",
-  /** #0072DF */
-  "Blue-500":
-    "var(--token-4eb4c550-4c1d-4e14-9a50-8ee56d7e4a42, rgb(0, 114, 223))",
-  /** #6E2EBF */
-  "Purple-500":
-    "var(--token-bc91c0ed-a459-4b88-80b5-8bde8cada448, rgb(110, 46, 191))",
-  /** #9A2EAD */
-  "Violet-500":
-    "var(--token-1bcb7d44-9bc5-4af3-9555-d0d423a033f6, rgb(154, 46, 173))",
-  /** #FF3390 */
-  "Pink-500":
-    "var(--token-f1b613f8-035a-43c8-b80a-7dc0a4fd7797, rgb(255, 51, 144))"
-});
+    /** #3448F7 */
+    "Brand-800": "var(--token-ad82e1ef-1d58-491a-b15f-dca38d6b8efa, rgb(52, 72, 247))",
+    /** #3570F4 */
+    "Brand-700": "var(--token-fcf6658b-174e-4b46-a231-494a2022300e, rgb(53, 112, 244))",
+    /** #3697F2 */
+    "Brand-600": "var(--token-ec615dec-db91-4509-9aa4-7e7e085be457, rgb(54, 151, 242))",
+    /** #8AC5ED */
+    "Brand-500": "var(--token-aeeab2af-1e28-479d-aa84-bd579d4169a8, rgb(138, 197, 237))",
+    /** #5D8CF6 */
+    "Brand-400": "var(--token-1f847b7c-4d90-4c25-87e3-4ed4f79e755f, rgb(93, 140, 246))",
+    /** #9AB7F9 */
+    "Brand-300": "var(--token-66ade0f7-390d-4113-ab34-f2932f302f34, rgb(154, 183, 249))",
+    /** #D7E2FD */
+    "Brand-200": "var(--token-f8ece739-c305-4b68-8a6e-8d66418a58d7, rgb(215, 226, 253))",
+    /** #F5F8FE */
+    "Brand-100": "var(--token-cf4881c9-b8c5-46cc-a37d-5a5b8ec96849, rgb(245, 248, 254))",
+    /** #303030 */
+    "Grey-800": "var(--token-e3a43a51-53ac-48e7-81a5-0baa0e96bd8b, rgb(48, 48, 48))",
+    /** #606060 */
+    "Grey-700": "var(--token-b368927d-c978-49d8-9561-c81769eaa2a5, rgb(96, 96, 96))",
+    /** #9A9A9A */
+    "Grey-600": "var(--token-a8a51b7f-4a04-4762-a305-109cc9e0104d, rgb(154, 154, 154))",
+    /** #D5D5D5 */
+    "Grey-500": "var(--token-3096b726-416b-4681-90e0-ef52272f36ff, rgb(213, 213, 213))",
+    /** #E8E8E8 */
+    "Grey-400": "var(--token-7b02582f-581f-4c94-b571-f49ce7c88556, rgb(232, 232, 232))",
+    /** #F2F2F2 */
+    "Grey-300": "var(--token-628127e1-30ea-4cea-b148-2311e3ed8054, rgb(242, 242, 242))",
+    /** #F8F8F8 */
+    "Grey-200": "var(--token-f5db83c5-dcff-423f-8386-31f3da4bbb77, rgb(248, 248, 248))",
+    /** #FFFFFF */
+    "Grey-100": "var(--token-200fcb65-d35b-4e99-be23-ab6ba9533e31, rgb(255, 255, 255))",
+    /** #EA0000 */
+    "Status-Danger": "var(--token-9887bc87-75b8-4c94-b231-92fbbb29c15f, rgb(234, 0, 0))",
+    /** #0066DF */
+    "Status-Information": "var(--token-43b6ab3f-8831-40dc-9f80-cae8379f29bf, rgb(0, 102, 223))",
+    /** #15C046 */
+    "Status-Success": "var(--token-e42aae84-a2ad-4e05-9eac-3bb69988e112, rgb(21, 192, 70))",
+    /** #F4CB00 */
+    "Status-Warning": "var(--token-a63c568b-4273-4330-af1b-488122d39e9f, rgb(244, 203, 0))",
+    /** #E21717 */
+    "Red-500": "var(--token-a6ccb1b7-1a52-4e3a-b566-dcb654f85ce5, rgb(226, 23, 23))",
+    /** #F17603 */
+    "Orange-500": "var(--token-cab4d733-8972-43e6-ac43-c6e503851f42, rgb(241, 118, 3))",
+    /** #FED800 */
+    "Yellow-500": "var(--token-8c5946b8-3a3b-476d-b734-c0246467629c, rgb(254, 216, 0))",
+    /** #87D812 */
+    "Lime-500": "var(--token-f7002086-deac-4d19-b2bf-26c8a384ac50, rgb(135, 216, 18))",
+    /** #20C05B */
+    "Green-500": "var(--token-fb34b6ff-f942-4c5b-b4e9-181e5a2b4c78, rgb(32, 192, 91))",
+    /** #19C6F4 */
+    "Sky-500": "var(--token-ddb92238-ac85-46cf-bfb8-541c522c82c8, rgb(25, 198, 244))",
+    /** #0072DF */
+    "Blue-500": "var(--token-48b223a7-9542-48a2-9901-e8553fd1075a, rgb(0, 114, 223))",
+    /** #6E2EBF */
+    "Purple-500": "var(--token-2ad2ad2c-fb68-44a7-bfcb-ce20b24ee0c8, rgb(110, 46, 191))",
+    /** #9A2EAD */
+    "Violet-500": "var(--token-1258a321-7b32-41b5-b76a-ac3c239884ca, rgb(154, 46, 173))",
+    /** #FF3390 */
+    "Pink-500": "var(--token-f104a732-1994-42d2-84ad-c2edeb1d07ec, rgb(255, 51, 144))",
+})
